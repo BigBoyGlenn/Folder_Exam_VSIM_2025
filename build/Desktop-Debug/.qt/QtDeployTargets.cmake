630f2048fe2a020bcefdfd4935ba56a65d972b3c
BigBoy <142876365+BigBoy@users.noreply.github.com>
2025-11-21 17:15:34 +0100

    fix gitignor

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtVulkan_FILE C:/Users/glenn/Documents/GitHub/Folder_Exam_VSIM_2025/build/Desktop-Debug/QtVulkan.exe)
-set(__QT_DEPLOY_TARGET_QtVulkan_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtVulkan_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Multimediad.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
